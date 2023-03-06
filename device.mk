0ea59eca1d33f6a8656182f4c4215336589c71ce
diamondsoul-750G <n.shetti65@gmail.com>
2023-03-06 10:50:33 +0530

    commit belongs to @roynatech2544

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-     android.hardware.biometrics.fingerprint@2.3-service.samsung
+     android.hardware.biometrics.fingerprint@2.1-service.samsung
 	
 # Permissions
 PRODUCT_COPY_FILES += \
