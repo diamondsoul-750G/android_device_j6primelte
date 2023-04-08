a7a642f50babc03cc98d05705ccf3a2111e5b2f0
diamondsoul-750G <n.shetti65@gmail.com>
2023-04-08 12:16:27 +0530

    fingerprint fixed

--- havoc_j6primelte.mk
+++ havoc_j6primelte.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Havoc stuff
+# Inherit some common havoc stuff
 $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit from j6primelte device
