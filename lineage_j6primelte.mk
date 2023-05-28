259a72df8ff7889b302f443c1a2fe985c9caa48a
diamondsoul-750G <n.shetti65@gmail.com>
2023-05-28 17:54:39 +0530

    upstream to a13

--- lineage_j6primelte.mk
+++ lineage_j6primelte.mk
@@ -24,7 +24,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := j6primelte
